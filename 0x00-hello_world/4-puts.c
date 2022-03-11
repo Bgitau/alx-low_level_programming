db2d7952189080f730c0e22c85507e054a63a542
Bgitau <001gitau@gmail.com>
2022-03-11 10:52:15 -0800

    kenya

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return:always 0(success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+ return (0);
 }
