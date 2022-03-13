261daa38f9de0bdc9a384bb1f132335bc3dbc0a2
Bgitau <001gitau@gmail.com>
2022-03-13 13:00:57 -0700

    more

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,19 @@ int main(void)
 {
 char c = 'a';
 
-  while (c <= 'z')
-    {
+while (c <= 'z')
+{
 putchar(c);
 c++;
-    }
+}
 
 c = 'A';
 
-  while (c <= 'Z')
-    {
+while (c <= 'Z')
+{
 putchar(c);
 c++;
-    }
+}
 
 putchar('\n');
 
