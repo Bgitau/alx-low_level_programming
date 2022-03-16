2712c8fc0cd0d7b8b9aab9c4357a3b6997c6cc68
Bgitau <001gitau@gmail.com>
2022-03-16 05:27:09 -0700

    yar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-  char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-  int i, j;
-  i = 0;
-  while (i < 10)
-    {
-      j = 0;
-      while (j < 26)
-	{
-	  _putchar(alphabet[j]);
-	  j++;
-	}
-      _putchar('\n');
-      i++;
-    }
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i, j;
+i = 0;
+while (i < 10)
+{
+j = 0;
+while (j < 26)
+{
+_putchar(alphabet[j]);
+j++;
+}
+_putchar('\n');
+i++;
+}
 }
