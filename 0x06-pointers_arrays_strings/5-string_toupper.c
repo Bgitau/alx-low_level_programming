1a845eeed170176ddb4bd277f78527da1a95ecfa
Bgitau <001gitau@gmail.com>
2022-03-24 14:02:21 -0700

    look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_tuopper -changes the letters of the string to uppercase
+ * @c: string pointer
+ * return: c
+ */
+
+char *string_toupper(char *c)
+{
+int a;
+
+for (a = 0; c[a] != '\0'; a++)
+{
+if (c[a]>96 &&c[a] < 123)
+{
+c[a] -=32;
+}
+}
+return (c);
+}
