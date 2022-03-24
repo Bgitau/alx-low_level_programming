d8fc68e055b5006eaba9fe9a43f11b4a1a898c3e
Bgitau <001gitau@gmail.com>
2022-03-24 16:44:08 -0700

    rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * rot13 - encodes a string using rot13
- *   * @s: input string.
- *    * Return: the pointer to dest.
- *     */
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
 char *rot13(char *s)
 {
@@ -15,12 +15,12 @@ while (*(s + count) != '\0')
 {
 for (i = 0; i < 52; i++)
 {
-																	if (*(s + count) == alphabet[i])
-																					{
-																										*(s + count) = rot13[i];
-																														break;
-																																	}
-																			}
+if (*(s + count) == alphabet[i])
+{
+*(s + count) = rot13[i];
+break;
+}
+}
 count++;
 }
 
