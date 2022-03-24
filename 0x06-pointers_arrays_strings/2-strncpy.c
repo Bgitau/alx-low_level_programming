e76c71bdf8c2223cec7de50d4268e00038da93ff
Bgitau <001gitau@gmail.com>
2022-03-24 15:41:56 -0700

    while

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
- for(i = 0; i < n && src[i] != '\0'; i++);
-dest[i] = src[i];
+i = 0;
 
-for (n > i; i++;)
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+
+while (i < n)
+{
 dest[i] = '\0';
+i++;
+}
 
 return (dest);
 }
