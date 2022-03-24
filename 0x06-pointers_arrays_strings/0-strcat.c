741a499eb3f100043af7b745c863dec1a03ce77f
Bgitau <001gitau@gmail.com>
2022-03-24 15:27:59 -0700

    leet

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
 #include "main.h"
-/**
- *_strcat - function that concatenates two strings.
- *@dest: pointer to destination string.
- *@src: pointer to source string.
- *
- *Return: pointer to destination string.
- */
-char *strcat(char *dest, char *src);
-{
-  int length, j; /* j will hold the length of the source */
 
-  length = 0;
-  while (dest[length] != '\0')
-    {
-      length++;
-    }
-  for (j = 0; src[j] != '\0'; j++, length++)
-    {
-      dest[length] = src[j];
-    }
-  dest[length] = '\0';
-  return (dest);
-}
+/**
+ *  * _strcat - concatenates two strings
+ *   * @dest: string to append to
+ *    * @src: string to add
+ *     *
+ *      * Return: a pointer to the resulting string
+ *       */
+char *_strcat(char *dest, char *src)
+{
+		int i, j;
+
+			i = 0;
+				j = 0;
+
+					while (dest[i] != '\0')
+								i++;
+
+						while (src[j] != '\0')
+								{
+											dest[i] = src[j];
+													j++;
+															i++;
+																}
+
+							dest[i] = '\0';
+
+								return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int i, c;
+int i, c;
 
-  for (i = 0; dest[i] != '\0'; i++)
-    ;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-  for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
-    {
-      dest[i] = src[c];
-    }
-  return (dest);
+for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+{
+dest[i] = src[c];
+}
+return (dest);
 }
