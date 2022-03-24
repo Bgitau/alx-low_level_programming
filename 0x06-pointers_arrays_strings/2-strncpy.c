3a0c7f8950081d721a3bb19267613e4553078e49
Bgitau <001gitau@gmail.com>
2022-03-24 13:33:13 -0700

    hey

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,10 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for(i = 0; i < n && src[i] != '\0'; i++)
+ for(i = 0; i < n && src[i] != '\0'; i++);
 dest[i] = src[i];
 
-for (n > i; i++)
+for (n > i; i++;)
 dest[i] = '\0';
 
 return (dest);
