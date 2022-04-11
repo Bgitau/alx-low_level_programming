141502d8e6a0e121fd10d95fab203abb22a60cc8
Bgitau <001gitau@gmail.com>
2022-04-11 16:22:56 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-printf("Name: %s\nAge: %f\nOwner: %s\n");
+printf("Name: %s\nAge: %f\nOwner: %s\n",
 (*d).name != NULL ? d->name : "(nil)",
 (*d).age,
-(*d).owner != NULL ? d->owner : "(nil)",
+(*d).owner != NULL ? d->owner : "(nil)");
 }
 }
